7d061b11bc2ffd11e9e372a2a2882ec0f8ac30a2
aldillon09 <aldillon09@gmail.com>
2024-12-19 15:08:18 +0100

    test updates

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from enum import Enum
-from typing import Optional
+from typing import Optional, Union, Dict
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-
 
 
 class Item(BaseModel):
@@ -10,6 +9,16 @@ class Item(BaseModel):
 	description: Optional[str] = None
 	price: float
 	tax: Optional[float] = None
+
+class ItemUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    tax: Optional[float] = None
+
+class ItemResponse(BaseModel):
+	message: str
+	item: Item
 
 
 items = {
@@ -39,31 +48,69 @@ async def list_items(
 
 
 # Read a single item by id
-@app.get("/items/{item_id}")
-async def get_item(item_id: int) -> Item:
+@app.get("/items/{item_id}", response_model=ItemResponse)
+async def get_item(
+	item_id: int
+) -> Dict[str, Union[str, Item]]:
 	if item_id not in items:
 		raise HTTPException(status_code=404, detail=f"Item with ID={item_id} not found.")
 	else:
-		return items[item_id]
+		return ItemResponse(
+			message="Item fetched successfully.",
+			item=items[item_id]
+		)
 
 
 # Create a new item
-@app.post("/create_items/")
-async def create_item(item: Item) -> list[Item]:
+@app.post("/create_items/", response_model=ItemResponse)
+async def create_item(
+	item: Item
+) -> Dict[str, Union[str, Item]]:
 	if any(existing_item.name == item.name for existing_item in items.values()):
 		raise HTTPException(status_code=400, detail=f"Item with name={item.name} already exists.")		
 	
 	new_id = max(items.keys()) + 1
 	items[new_id] = item
 	
-	return [item for item in items.values()]
-
+	return ItemResponse(
+		message="Product created successfully",
+		item=items[new_id]
+	)
 
 
 # Update an item
+@app.put("/update/{item_id}", response_model=ItemResponse)
+async def update_items(
+	item_id: int,
+	item_update: ItemUpdate
+) -> Dict[str, Union[str, Item]]:
+	if item_id not in items:
+		raise HTTPException(status_code=400, detail=f"Item with ID={item_id} does not exist.")
+	
+	current_item = items[item_id]
+	update_data = item_update.model_dump(exclude_unset=True)
 
+	for field, value in update_data.items():
+		setattr(current_item, field, value)
+	
+	return ItemResponse(
+		message="Product updated successfully",
+		item=current_item
+	)
 
 
 # Delete an item
+@app.delete("/delete/{item_id}", response_model=ItemResponse)
+async def delete_item(
+	item_id: int
+) -> Dict[str, Union[str, Item]]:
+	if item_id not in items:
+		raise HTTPException(status_code=400, detail=f"Item with ID={item_id} does not exist.")
+	deleted_item = items[item_id]
+	items.pop(item_id)
 
+	return ItemResponse(
+		message="Item successfully deleted.",
+		item=deleted_item
+	)
 
